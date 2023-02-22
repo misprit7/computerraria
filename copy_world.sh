f2adb4f2479cddf32a7160345e94462c38c26f79
misprit7 <xandernaumenko@gmail.com>
2023-02-22 10:01:44 -0800

    Added doc

--- copy_world.sh
+++ copy_world.sh
@@ -1,1 +1,11 @@
-cp ~/.local/share/Terraria/tModLoader/Worlds/computer.wld .
+case $1 in
+    -t|--to)
+        cp ./computer.wld ~/.local/share/Terraria/tModLoader/Worlds/computer.wld
+        ;;
+    -f|--from)
+        cp ~/.local/share/Terraria/tModLoader/Worlds/computer.wld .
+        ;;
+    *)
+        "Supply either --to or --from to copy either from or to the Terraria world files" 
+        ;;
+esac        
