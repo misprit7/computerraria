b309111f25f0943cc468a4799e1006944e822ab3
misprit7 <xandernaumenko@gmail.com>
2023-03-29 01:35:35 -0700

    Improved docs

--- app/template/copy_bin.sh
+++ app/template/copy_bin.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/zsh
+#!/usr/bin/sh
 
 # Helper script to copy file to txt file format WireHead expects
 # Example: ./copy_bin.sh /tmp/in.txt
@@ -8,8 +8,8 @@ cargo build --release
 
 # Convert from elf to bin
 # Fancy glob matching! https://zsh.sourceforge.io/Doc/Release/Expansion.html#Glob-Qualifiers
-# If you're not zsh then sucks to suck, just hardcode your project name
-rust-objcopy -O binary ./target/riscv32i-unknown-none-elf/release/*(x.) /tmp/rust.bin 
+# rust-objcopy -O binary ./target/riscv32i-unknown-none-elf/release/*(x.) /tmp/rust.bin 
+rust-objcopy -O binary ./target/riscv32i-unknown-none-elf/release/template /tmp/rust.bin 
 
 # Convert from bin to txt
 hexdump -ve '1/1 "%.2x "' /tmp/rust.bin | head -c -1 > $1
