61f9d18f7a5cb78e2b99b736b5a6de9f3ff77ce1
misprit7 <xandernaumenko@gmail.com>
2023-04-01 14:04:45 -0700

    Removed outdated scripts

--- app/game-of-life/binutils.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/sh
-
-# Script to call binutils without having to remember a million cli options
-# e.g. ./binutils.sh objdump -d or ./binutils.sh size
-cargo $1 --release --target riscv32i-unknown-none-elf --bin game-of-life -- "${@:2}"
-
