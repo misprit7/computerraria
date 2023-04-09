69c31d1bbfa7def19fadc9394f049bde7b8eb873
misprit7 <xandernaumenko@gmail.com>
2023-04-09 16:41:52 -0700

    Switched to fixed point

--- app/doom3d/src/raycaster.rs
+++ app/doom3d/src/raycaster.rs
@@ -1,41 +1,42 @@
 use core::cmp::{max, min};
 use tdriver::graphics;
-use libm;
+use fixed::{types::I16F16, FixedI32};
+use cordic;
 
-const PI: f32 = 3.1415926;
+const PI: I16F16 = FixedI32::PI;
 pub const MAP_WIDTH: usize = 5;
 pub const MAP_HEIGHT: usize = 7;
 
 pub struct Raycaster {
     map: [[bool; MAP_WIDTH]; MAP_HEIGHT],
-    tan_half_fov: f32,
+    tan_half_fov: I16F16,
 }
 
 struct RayHit {
-    length: f32,
+    length: I16F16,
     idx_x: usize,
     idx_y: usize,
     normal_along_x: bool,
 }
 
 impl Raycaster {
-    pub fn new(map: [[bool; MAP_WIDTH]; MAP_HEIGHT], fov_deg: f32) -> Self {
-        let fov_rad = fov_deg * PI / 180.0;
+    pub fn new(map: [[bool; MAP_WIDTH]; MAP_HEIGHT], fov_deg: I16F16) -> Self {
+        let fov_rad = fov_deg * PI / I16F16::from_num(180.0);
         Raycaster {
             map,
-            tan_half_fov: libm::tanf(fov_rad / 2.0),
+            tan_half_fov: cordic::tan(fov_rad / I16F16::from_num(2.0)),
         }
     }
 
     pub fn render(
         &self,
-        start_x: f32,
-        start_y: f32,
-        cam_angle_rad: f32,
+        start_x: I16F16,
+        start_y: I16F16,
+        cam_angle_rad: I16F16,
         pixels: &mut [[bool; graphics::WIDTH]; graphics::HEIGHT],
     ) {
         let mut last_hit = RayHit {
-            length: 0.0,
+            length: I16F16::const_from_int(0),
             idx_x: 0,
             idx_y: 0,
             normal_along_x: false,
@@ -45,12 +46,12 @@ impl Raycaster {
         let mut last_on_wall = false;
 
         for x_pixel in 0..graphics::WIDTH {
-            let screen_coord = (x_pixel as f32) / (graphics::WIDTH as f32);
+            let screen_coord = (I16F16::from_num(x_pixel)) / (I16F16::from_num(graphics::WIDTH));
             let ray_angle_rad = self.screen_coord_to_angle_rad(screen_coord) + cam_angle_rad;
             let hit = self.cast_ray(start_x, start_y, ray_angle_rad);
 
             if let Some(hit) = hit {
-                let height: i32 = (graphics::HEIGHT as f32 / hit.length) as i32;
+                let height: i32 = (I16F16::from_num(graphics::HEIGHT) / hit.length).to_num::<i32>();
                 let top: usize = min(
                     (graphics::HEIGHT as i32 / 2) + (height / 2),
                     graphics::HEIGHT as i32 - 1,
@@ -102,38 +103,38 @@ impl Raycaster {
         }
     }
 
-    fn screen_coord_to_angle_rad(&self, screen_coord: f32) -> f32 {
+    fn screen_coord_to_angle_rad(&self, screen_coord: I16F16) -> I16F16 {
         // ooh magic
-        libm::atanf((2.0 * screen_coord - 1.0) * self.tan_half_fov)
+        cordic::atan((2 * screen_coord - I16F16::const_from_int(1)) * self.tan_half_fov)
     }
 
     // DDA algorithm (https://www.youtube.com/watch?v=NbSee-XM7WA&ab_channel=javidx9)
-    fn cast_ray(&self, start_x: f32, start_y: f32, ray_angle_rad: f32) -> Option<RayHit> {
-        let dir_x = libm::cosf(ray_angle_rad);
-        let dir_y = libm::sinf(ray_angle_rad);
-        let ray_unit_step_size_x = libm::fabsf(1.0 / dir_x); // Length of step if moving 1 unit in x
-        let ray_unit_step_size_y = libm::fabsf(1.0 / dir_y); // Length of step if moving 1 unit in y
+    fn cast_ray(&self, start_x: I16F16, start_y: I16F16, ray_angle_rad: I16F16) -> Option<RayHit> {
+        let dir_x = cordic::cos(ray_angle_rad);
+        let dir_y = cordic::sin(ray_angle_rad);
+        let ray_unit_step_size_x = dir_x.wrapping_recip().abs(); // Length of step if moving 1 unit in x
+        let ray_unit_step_size_y = dir_y.wrapping_recip().abs(); // Length of step if moving 1 unit in y
 
         // Length of ray if next step is 1 unit in x (account for off-grid start)
         let mut ray_length_x = if dir_x >= 0.0 {
-            libm::ceilf(start_x) - start_x
+            start_x.ceil() - start_x
         } else {
-            start_x - libm::floorf(start_x)
+            start_x - start_x.floor()
         } * ray_unit_step_size_x;
 
         // Length of ray if next step is 1 unit in y (account for off-grid start)
         let mut ray_length_y = if dir_y >= 0.0 {
-            libm::ceilf(start_y) - start_y
+            start_y.ceil() - start_y
         } else {
-            start_y - libm::floorf(start_y)
+            start_y - start_y.floor()
         } * ray_unit_step_size_y;
 
         let step_x = if dir_x > 0.0 {1} else {-1};
         let step_y = if dir_y > 0.0 {1} else {-1};
-        let mut idx_x = start_x as i32;
-        let mut idx_y = start_y as i32;
+        let mut idx_x = start_x.to_num::<i32>();
+        let mut idx_y = start_y.to_num::<i32>();
 
-        let mut ray_length = 0.0;
+        let mut ray_length = I16F16::const_from_int(0);
         loop {
             if self.valid_map_idx(idx_x, idx_y) {
                 if self.map[idx_y as usize][idx_x as usize] {
@@ -141,8 +142,8 @@ impl Raycaster {
                     let ray_hit_x = ray_length * dir_x;
                     let ray_hit_y = ray_length * dir_y;
 
-                    let x_diff = libm::fabsf(ray_hit_x - libm::roundf(ray_hit_x));
-                    let y_diff = libm::fabsf(ray_hit_y - libm::roundf(ray_hit_y));
+                    let x_diff = (ray_hit_x - ray_hit_x.round()).abs();
+                    let y_diff = (ray_hit_y - ray_hit_y.round()).abs();
                     let normal_along_x = x_diff < y_diff;
 
                     return Some(RayHit {
