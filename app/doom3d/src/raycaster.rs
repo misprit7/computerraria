678fa2600e14cceb3d9807d27e8e40dbb2443ebc
Gus Tahara-Edmonds <gtaharaedmonds@gmail.com>
2023-04-09 00:27:13 -0700

    demo map

--- app/doom3d/src/raycaster.rs
+++ app/doom3d/src/raycaster.rs
@@ -1,24 +1,20 @@
-use core::cmp::{min, max};
+use core::cmp::{max, min};
 use tdriver::graphics;
-use tdriver::graphics::HEIGHT;
-use tdriver::graphics::WIDTH;
 
 const PI: f32 = 3.1415926;
-
-pub const MAP_WIDTH: usize = 24;
-pub const MAP_HEIGHT: usize = 24;
-pub const STEP_SIZE: f32 = 0.001;
+pub const MAP_WIDTH: usize = 5;
+pub const MAP_HEIGHT: usize = 7;
 
 pub struct Raycaster {
     map: [[bool; MAP_WIDTH]; MAP_HEIGHT],
     tan_half_fov: f32,
 }
 
-#[derive(Debug, PartialEq)]
-enum WallType {
-    Horizontal, // Normal on y-axis
-    Vertical, // Normal on x-axis
-    None, // No wall (looking into void)
+struct RayHit {
+    length: f32,
+    idx_x: usize,
+    idx_y: usize,
+    normal_along_x: bool,
 }
 
 impl Raycaster {
@@ -30,36 +26,51 @@ impl Raycaster {
         }
     }
 
-    pub fn render(&self, start_x: f32, start_y: f32, cam_angle_rad: f32, pixels: &mut [[bool; graphics::WIDTH]; graphics::HEIGHT]) {
-        let mut last_wall_type = WallType::None;
+    pub fn render(
+        &self,
+        start_x: f32,
+        start_y: f32,
+        cam_angle_rad: f32,
+        pixels: &mut [[bool; graphics::WIDTH]; graphics::HEIGHT],
+    ) {
+        let mut last_hit = RayHit {
+            length: 0.0,
+            idx_x: 0,
+            idx_y: 0,
+            normal_along_x: false,
+        };
         let mut last_top = 0;
         let mut last_bottom = 0;
+        let mut last_on_wall = false;
 
         for x_pixel in 0..graphics::WIDTH {
             let screen_coord = (x_pixel as f32) / (graphics::WIDTH as f32);
             let ray_angle_rad = self.screen_coord_to_angle_rad(screen_coord) + cam_angle_rad;
             let hit = self.cast_ray(start_x, start_y, ray_angle_rad);
 
-            if let Some((distance, wall_type)) = hit {
-                let height: i32 = (graphics::HEIGHT as f32 / distance) as i32;
-                let top: usize = min((graphics::HEIGHT as i32 / 2) + (height / 2), graphics::HEIGHT as i32 - 1) as usize;
+            if let Some(hit) = hit {
+                let height: i32 = (graphics::HEIGHT as f32 / hit.length) as i32;
+                let top: usize = min(
+                    (graphics::HEIGHT as i32 / 2) + (height / 2),
+                    graphics::HEIGHT as i32 - 1,
+                ) as usize;
                 let bottom: usize = max((graphics::HEIGHT as i32 / 2) - (height / 2), 0) as usize;
-                
-                if wall_type != last_wall_type && x_pixel > 0 {
-                    // hit a wall corner, draw a vertical line
-                    //println!("Hit wall corner");
 
+                let adjacent_x = (hit.idx_x as i32 - last_hit.idx_x as i32).abs() <= 1;
+                let adjacent_y = (hit.idx_y as i32 - last_hit.idx_y as i32).abs() <= 1;
+
+                if (!adjacent_x
+                    || !adjacent_y
+                    || hit.normal_along_x != last_hit.normal_along_x
+                    || !last_on_wall)
+                    && x_pixel > 0
+                {
+                    // hit a wall corner, draw a vertical line
                     for y_pixel in 0..graphics::HEIGHT {
-                        pixels[y_pixel][x_pixel] = if y_pixel >= bottom && y_pixel <= top {
-                            true
-                        } else {
-                            false
-                        };
+                        pixels[y_pixel][x_pixel] = y_pixel >= bottom && y_pixel <= max(top, last_top);
                     }
                 } else {
                     // hit a wall, draw top and bottom only
-                    //println!("Hit wall but not corner");
-                    
                     for y_pixel in 0..graphics::HEIGHT {
                         pixels[y_pixel][x_pixel] = false;
                     }
@@ -67,95 +78,98 @@ impl Raycaster {
                     pixels[top][x_pixel] = true;
                     pixels[bottom][x_pixel] = true;
                 }
-                
-                //println!("Hit data! Wall type: {:?}", wall_type);
 
-                last_wall_type = wall_type;
+                last_hit = hit;
                 last_top = top;
                 last_bottom = bottom;
-            }
-            else {
-                if last_wall_type != WallType::None {
+                last_on_wall = true;
+            } else {
+                if last_on_wall {
                     // hit blank space after seeing wall, draw a vertical line
-                    //println!("Hit blank space first time, draw corner");
-                    
                     for y_pixel in 0..graphics::HEIGHT {
-                        pixels[y_pixel][x_pixel] = if y_pixel >= last_bottom && y_pixel <= last_top {
-                            true
-                        } else {
-                            false
-                        };
+                        pixels[y_pixel][x_pixel] = y_pixel >= last_bottom && y_pixel <= last_top;
                     }
-                }
-                else {
+                } else {
                     // hit blank space, draw nothing
-                    //println!("Hit blank space, draw nothing");
-
                     for y_pixel in 0..graphics::HEIGHT {
                         pixels[y_pixel][x_pixel] = false;
                     }
                 }
 
-                last_wall_type = WallType::None;
+                last_on_wall = false;
             }
         }
     }
-    
+
     fn screen_coord_to_angle_rad(&self, screen_coord: f32) -> f32 {
         // ooh magic
         ((2.0 * screen_coord - 1.0) * self.tan_half_fov).atan()
     }
 
-    fn cast_ray(&self, start_x: f32, start_y: f32, ray_angle_rad: f32) -> Option<(f32, WallType)> {
+    // DDA algorithm (https://www.youtube.com/watch?v=NbSee-XM7WA&ab_channel=javidx9)
+    fn cast_ray(&self, start_x: f32, start_y: f32, ray_angle_rad: f32) -> Option<RayHit> {
         let dir_x = ray_angle_rad.cos();
         let dir_y = ray_angle_rad.sin();
+        let ray_unit_step_size_x = 1.0 / dir_x.abs(); // Length of step if moving 1 unit in x
+        let ray_unit_step_size_y = 1.0 / dir_y.abs(); // Length of step if moving 1 unit in y
 
-        let mut pos_x = start_x;
-        let mut pos_y = start_y;
+        // Length of ray if next step is 1 unit in x (account for off-grid start)
+        let mut ray_length_x = if dir_x >= 0.0 {
+            start_x.ceil() - start_x
+        } else {
+            start_x - start_x.floor()
+        } * ray_unit_step_size_x;
 
+        // Length of ray if next step is 1 unit in y (account for off-grid start)
+        let mut ray_length_y = if dir_y >= 0.0 {
+            start_y.ceil() - start_y
+        } else {
+            start_y - start_y.floor()
+        } * ray_unit_step_size_y;
+
+        let step_x = dir_x.signum() as i32;
+        let step_y = dir_y.signum() as i32;
+        let mut idx_x = start_x as i32;
+        let mut idx_y = start_y as i32;
+
+        let mut ray_length = 0.0;
         loop {
-            if let Some((idx_x, idx_y)) = self.loc_to_idx(pos_x, pos_y) {
-                if self.map[idx_y][idx_x] {
-                    // ray has collided with a wall
-                    let dx = pos_x - start_x;
-                    let dy = pos_y - start_y;
-                    let distance = (dx * dx + dy * dy).sqrt();
+            if self.valid_map_idx(idx_x, idx_y) {
+                if self.map[idx_y as usize][idx_x as usize] {
+                    // ray has collided with a wall!
+                    let ray_hit_x = ray_length * dir_x;
+                    let ray_hit_y = ray_length * dir_y;
 
-                    let x_diff = (pos_x - pos_x.round()).abs();
-                    let y_diff = (pos_y - pos_y.round()).abs();
-                    let wall_type = if x_diff < y_diff {
-                        WallType::Vertical  
-                    } else {
-                        WallType::Horizontal
-                    };
+                    let x_diff = (ray_hit_x - ray_hit_x.round()).abs();
+                    let y_diff = (ray_hit_y - ray_hit_y.round()).abs();
+                    let normal_along_x = x_diff < y_diff;
 
-                    return Some((distance, wall_type));
+                    return Some(RayHit {
+                        length: ray_length,
+                        idx_x: idx_x as usize,
+                        idx_y: idx_y as usize,
+                        normal_along_x,
+                    });
                 }
-            }
-            else {
-                // ray has left map area
+            } else {
+                // ray has left map area, exit
                 return None;
             }
 
-            pos_x += dir_x * STEP_SIZE;
-            pos_y += dir_y * STEP_SIZE;
+            // Walk along shortest ray
+            if ray_length_x <= ray_length_y {
+                idx_x += step_x;
+                ray_length = ray_length_x;
+                ray_length_x += ray_unit_step_size_x;
+            } else {
+                idx_y += step_y;
+                ray_length = ray_length_y;
+                ray_length_y += ray_unit_step_size_y;
+            }
         }
     }
 
-    fn loc_to_idx(&self, pos_x: f32, pos_y: f32) -> Option<(usize, usize)> {
-        let idx_x = pos_x as usize;
-        let idx_y = pos_y as usize;
-
-        if [
-            pos_x < 0.0,
-            pos_y < 0.0,
-            idx_x >= MAP_WIDTH,
-            idx_y >= MAP_HEIGHT,
-        ].iter().any(|&x| x) {
-            None
-        }
-        else {
-            Some((idx_x, idx_y))
-        }
+    fn valid_map_idx(&self, pos_x: i32, pos_y: i32) -> bool {
+        pos_x >= 0 && pos_y >= 0 && pos_x < MAP_WIDTH as i32 && pos_y < MAP_HEIGHT as i32
     }
 }
