70113dce77648f2b69e2376a4066b0120fa57b74
misprit7 <xandernaumenko@gmail.com>
2024-06-25 13:47:52 -0700

    Added twld to make colored pbs persistent

--- copy-world.sh
+++ copy-world.sh
@@ -7,11 +7,13 @@
 case $1 in
     -t|--to)
         cp $(dirname "$0")/computer.wld ~/.local/share/Terraria/tModLoader/Worlds/computer.wld
+        cp $(dirname "$0")/computer.twld ~/.local/share/Terraria/tModLoader/Worlds/computer.twld
         ;;
     -f|--from)
         cp ~/.local/share/Terraria/tModLoader/Worlds/computer.wld $(dirname "$0")
+        cp ~/.local/share/Terraria/tModLoader/Worlds/computer.twld $(dirname "$0")
         ;;
     *)
-        "Supply either --to or --from to copy either from or to the Terraria world files" 
+        "Supply either --to or --from to copy either to or from the Terraria world files" 
         ;;
 esac        
